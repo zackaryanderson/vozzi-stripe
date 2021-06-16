a669da4383421b2ec4074a117de6b695c703f3a7
zanderson1998 <zanderson1998@yahoo.com>
2021-06-16 17:14:54 -0600

    add basic files and basic setup w/ notes

---
+++ script.js
@@ -0,0 +1,6 @@
+// read selected buttons to see what donation amount the user would like using a global state
+
+// relocate to checkout page 
+    //checkout page sends money directly to client instead of to vozzi account
+
+// after checkout relocate back to index and add pop-up thanking the customer for their donation to the client
