33a92e675a9f8bab6d13adbc6f11f04e13866c32
zanderson1998 <zanderson1998@yahoo.com>
2021-06-16 18:02:54 -0600

    basic html landing page

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // read selected buttons to see what donation amount the user would like using a global state
 
+//if reoccuring payment button selected then dynamically change the page content
+
 // relocate to checkout page 
     //checkout page sends money directly to client instead of to vozzi account
 
 // after checkout relocate back to index and add pop-up thanking the customer for their donation to the client
 
 //main functions
+
